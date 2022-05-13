84e9ba3e61199691d3030943147f90deebaec091
jazzim110 <jazzim4762@gmail.com>
2022-05-13 16:34:07 +0900

    ...

--- src/app.py
+++ src/app.py
@@ -7,18 +7,7 @@ import time
 def create_app():
   app = Flask(__name__)
 
-  metrics = PrometheusMetrics(app)
-  metrics.info('app_info', 'Application info', version='1.0.0')
-
-  metrics.register_default(
-    metrics.counter(
-      'by_path_counter', 'Request count by request paths',
-      labels={'path': lambda: request.path}
-    )
-  )
-
   @app.route("/healthz")
-  @metrics.exclude_all_metrics()
   def healthz():
     return {
       "status": 200,
@@ -31,9 +20,6 @@ def create_app():
 
   @app.route("/hello/<username>")
   def hello_user(username):
-    print("username: ", username)
-    if username == 'slow':
-        time.sleep(randrange(1, 5))
     return jsonify({"message": "Hello %s\n" % username}), 200
 
   return app
