9fef5d6f5780181fa8e7f83741a370b612c61a91
jazzim110 <jazzim4762@gmail.com>
2022-05-27 02:53:04 +0000

    change jenkinsfile

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 REGION="ap-northeast-2"
-ACCOUNT_ID="657976307134"
+ACCOUNT_ID="802844773267"
 ECR_REPOSITORY="${ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com"
-ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/demo"
-ECR_DOCKER_TAG="1.0.0"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/demo-jh"
+ECR_DOCKER_TAG="2.0.0"
 
 aws ecr get-login-password --region ${REGION} \
   | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
